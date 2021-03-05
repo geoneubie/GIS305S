0b10ca28b5a02ef055d0f2eb9671f0042cd2289e
morri <morrisaj18@gmail.com>
2021-03-04 20:01:23 -0700

    needed to except a type error to return partial results, and instruct user to check data type

--- assignment7/BasicMap.py
+++ assignment7/BasicMap.py
@@ -54,8 +54,8 @@ class BasicMap:
 if __name__ == "__main__":
     my_map = BasicMap("-105.2705", "40.015", "0.5", "0.25")
     my_map.describe()
-
+try:
     print("Calculating bounds...")
     my_map.get_bounds()
-
-
+except: TypeError
+print("Error in get_bounds - check input, values must be numbers!")
