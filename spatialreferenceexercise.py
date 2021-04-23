a3d1f3e79bf5033b52f640e0855fe1c344a02466
Jodi Jade Trayler <jtrayler@student.cccs.edu>
2021-04-22 19:32:17 -0600

    Definition Query

--- spatialreferenceexercise.py
+++ spatialreferenceexercise.py
@@ -6,8 +6,19 @@ for sr_string in srs:
     sr_object = arcpy.SpatialReference(sr_string)
     print("{0.centralMeridian}   {0.name}  {0.PCSCode}".format(sr_object))
 
-aprx = arcpy.mp.ArcGISProject(r"C:\Users\Owner\Desktop\GIS305\GIS305Ass#1\GIS305Ass#1.aprx")
+aprx = arcpy.mp.ArcGISProject(r"C:\Users\Owner\Documents\ArcGIS\Projects\ClassExerciseLesson#1\ClassExerciseLesson#1"
+                              r".aprx")
 map_doc = aprx.listMaps()[0]
 
 state_plane_noco = arcpy.SpatialReference(3743)
 map_doc.spatialReference = state_plane_noco
+
+lyr = map_doc.listLayers("U.S. States (Generalized)")[0]
+# Get the symbology.
+sym = lyr.symbology
+
+sym.renderer.symbol.color = {'RGB': [255, 0, 0, 100]}
+sym.renderer.symbol.outlineColor = {'RGB': [0, 0, 0, 100]}
+lyr.symbology = sym
+lyr.transparency = 50
+aprx.save()
