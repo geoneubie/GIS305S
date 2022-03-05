ce4d21919b1e21e2dc30fda561b87faa3a46d83e
CMyers0610 <lbcem@aol.com>
2022-03-05 11:49:24 -0700

    Error Fix- Remove quotes from around numbers so program reads as number not text.

--- assignment7/BasicMap.py
+++ assignment7/BasicMap.py
@@ -52,7 +52,7 @@ class BasicMap:
 
 
 if __name__ == "__main__":
-    my_map = BasicMap("-105.2705", "40.015", "0.5", "0.25")
+    my_map = BasicMap(-105.2705, 40.015, 0.5, 0.25)
     my_map.describe()
 
     try:
