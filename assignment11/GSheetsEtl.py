10722aab7d75ba9d94c73d65c0c07d55d2e31e03
hcvin <hvinson2@student.cccs.edu>
2023-04-24 14:52:11 -0600

    Added GSheetsEtl

---
+++ assignment11/GSheetsEtl.py
@@ -0,0 +1,11 @@
+from SpatialEtl import SpatialEtl
+
+
+class GSheetsEtl(SpatialEtl):
+    def __int__(self, remote, local_dir, data_format, destination):
+        super().__int__(remote, local_dir, data_format, destination)
+
+    def process(self):
+        super().extract()
+        super().transform()
+        super().load()
