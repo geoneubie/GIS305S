d3da8bd87d50ae982aa96e906a3b14bd4a67f564
hcvin <hvinson2@student.cccs.edu>
2023-05-07 17:07:48 -0600

    Final Project set up.

---
+++ WNVOutbreak/FinalProject/etl/GSheetsEtl.py
@@ -0,0 +1,67 @@
+import csv
+import requests
+import arcpy
+import json
+from SpatialEtl import SpatialEtl
+
+
+class GSheetsEtl(SpatialEtl):
+    # creates a class called GsheetsETL that uses the SpatialEtl class
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+    def extract(self):
+        # downloads and pulls out the data
+        print("Extracting addresses from google form spreadsheet")
+        r = requests.get(self.config_dict.get('remote_url'))
+        r.encoding = "utf-8"
+        data = r.text
+        with open(rf"{self.config_dict.get('proj_dir')}addresses.csv", "w") as output_file:
+            output_file.write(data)
+
+    def transform(self):
+        print("Add City, State")
+
+        transformed_file = open(f"{self.config_dict.get('proj_dir')}new_addresses.csv", "w")
+        transformed_file.write("X,Y,Type\n")
+        # opens the file from the extract function, geocodes the data and adds it to a new CSV file
+        with open(f"{self.config_dict.get('proj_dir')}addresses.csv", "r") as partial_file:
+            csv_dict = csv.DictReader(partial_file, delimiter=',')
+            for row in csv_dict:
+                address = row["Street Address"] + " Boulder CO"
+                print(address)
+                geocode_url = self.config_dict.get('geocoder_prefix_url') + address + self.config_dict.get \
+                    ('geocoder_suffix_url')
+                print(geocode_url)
+                r = requests.get(geocode_url)
+
+                resp_dict = r.json()
+                x = resp_dict['result']['addressMatches'][0]['coordinates']['x']
+                y = resp_dict['result']['addressMatches'][0]['coordinates']['y']
+                transformed_file.write(f"{x},{y},Residential\n")
+
+        transformed_file.close()
+
+    def load(self):
+        # Description: Creates a point feature class from input table
+
+        # Set environment settings
+        arcpy.env.workspace = (f"{self.config_dict.get('proj_dir')}arcgis\westnileoutbreak\WestNileOutbreak.gdb")
+        arcpy.env.overwriteOutput = True
+
+        # Set the local variables
+        in_table = (f"{self.config_dict.get('proj_dir')}new_addresses.csv")
+        out_feature_class = "avoid_points"
+        x_coords = "X"
+        y_coords = "Y"
+
+        # Make the XY event layer...
+        arcpy.management.XYTableToPoint(in_table, out_feature_class, x_coords, y_coords)
+
+        # Print the total rows
+        print(arcpy.GetCount_management(out_feature_class))
+
+    def process(self):
+        self.extract()
+        self.transform()
+        self.load()
